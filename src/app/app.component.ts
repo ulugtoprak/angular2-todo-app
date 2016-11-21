f9ebb4fee580d890aa3b89bc7dd166e0a3be2f58
Enda Lee <enda.lee@ittdublin.ie>
2016-11-21 10:49:14 +0000

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'i took the title out of the html';
 }
